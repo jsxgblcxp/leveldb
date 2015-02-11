6e2f615f858ebe4e930e3e469d0323915ac70bad
gb <gb@localhost.localdomain>
2015-02-11 09:25:34 +0000

    just take vim to do gg=G to format the *.h *.cc file

--- util/mutexlock.h
+++ util/mutexlock.h
@@ -10,30 +10,30 @@
 
 namespace leveldb {
 
-// Helper class that locks a mutex on construction and unlocks the mutex when
-// the destructor of the MutexLock object is invoked.
-//
-// Typical usage:
-//
-//   void MyClass::MyMethod() {
-//     MutexLock l(&mu_);       // mu_ is an instance variable
-//     ... some complex code, possibly with multiple return paths ...
-//   }
+        // Helper class that locks a mutex on construction and unlocks the mutex when
+        // the destructor of the MutexLock object is invoked.
+        //
+        // Typical usage:
+        //
+        //   void MyClass::MyMethod() {
+        //     MutexLock l(&mu_);       // mu_ is an instance variable
+        //     ... some complex code, possibly with multiple return paths ...
+        //   }
 
-class SCOPED_LOCKABLE MutexLock {
- public:
-  explicit MutexLock(port::Mutex *mu) EXCLUSIVE_LOCK_FUNCTION(mu)
-      : mu_(mu)  {
-    this->mu_->Lock();
-  }
-  ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
+        class SCOPED_LOCKABLE MutexLock {
+                public:
+                        explicit MutexLock(port::Mutex *mu) EXCLUSIVE_LOCK_FUNCTION(mu)
+                                : mu_(mu)  {
+                                        this->mu_->Lock();
+                                }
+                        ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
 
- private:
-  port::Mutex *const mu_;
-  // No copying allowed
-  MutexLock(const MutexLock&);
-  void operator=(const MutexLock&);
-};
+                private:
+                        port::Mutex *const mu_;
+                        // No copying allowed
+                        MutexLock(const MutexLock&);
+                        void operator=(const MutexLock&);
+        };
 
 }  // namespace leveldb
 
